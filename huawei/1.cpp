62c59a64e7af7cf0ab115564abea9dec2d11b725
friendhy <2944208156@qq.com>
2023-02-21 19:38:15 +0800

    commit test

---
+++ huawei/1.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file 1.cpp
+ * @author Friendy (friend0@qq.com)
+ * @date 2023-02-21
+ * @version 0.1
+ * @brief nowcoder practice
+ * @gitee https://gitee.com/friendhy/nowcoder
+ * @copyright Copyright (c) 2023
+ */
+#include<iostream>
+using namespace std;
+#define pii pair<int,int>
+#define ll long long
+#define pll pair<ll,ll>
+#define file(x) freopen(#x ".in", "r", stdin); freopen(#x ".out", "w", stdout);
+#define rep(i,s,t) for(int i=s;i<=t;i++)
+#define dwn(i,s,t) for(int i=s;i>=t;i--)
+#define ite(it,s,e) for(auto it=s;it!=e;++it)
+string s;
+int main(){
+    ios::sync_with_stdio(false);
+    // file(data);
+    while(cin>>s);
+    cout<<s.size();
+    return 0;
+}
